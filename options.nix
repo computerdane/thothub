4b3d4020a738ac0ea9451af5aedb7d8e3ebcb08f
Dane Rieber <danerieber@gmail.com>
2025-04-25 16:55:54 -0500

    shell option

--- options.nix
+++ options.nix
@@ -36,4 +36,14 @@ lib: with lib; {
     description = "Specifies the hashed password for the user. See {option}`users.users.<name>.hashedPassword`";
   };
 
+  shell = mkOption {
+    description = "User's preferred shell";
+    default = "fish";
+    type = types.enum [
+      "bash"
+      "fish"
+      "zsh"
+    ];
+  };
+
 }
