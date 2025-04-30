79509d10d502236e6b05f8d396e1d604e4592a95
Dane Rieber <danerieber@gmail.com>
2025-04-30 12:06:07 -0500

    document toMinecraftOps; add tests for lib

--- check.sh
+++ check.sh
@@ -1,8 +1,10 @@
 # check.sh
 #
-# Ensures that the "thots" NixOS module has a valid configuration.
+# Ensures that the "thots" NixOS module has a valid configuration and that all
+# lib tests pass.
 
 set -eou pipefail
 
 nix flake check
 nix eval .#evalModule.config.thots "$@" >/dev/null
+nix eval .#lib.testsPassed "$@" >/dev/null
--- lib.nix
+++ lib.nix
@@ -1,9 +1,40 @@
+/*
+  lib.nix
+
+  Exports some helper functions that handle common use cases for end users.
+*/
+
 { lib }:
 let
   inherit (builtins) map;
   inherit (lib) flatten;
 in
-{
+rec {
+  /**
+    Maps a list of thots to a list that matches the schema of Minecraft's
+    ops.json. Use builtins.toJSON to convert the result to actual JSON.
+
+    # Inputs
+
+    `thots`
+
+    : A list of thots from {option}`config.thots`
+
+    # Type
+
+    ```
+    toMinecraftOps :: [AttrSet] -> [AttrSet]
+    ```
+
+    # Example
+    :::{.example}
+    ## `thothub.lib.toMinecraftOps` usage example
+
+    ```
+    toMinecraftOps (with config.thots; [ dane scott ])
+    => [ { bypassesPlayerLimit = true; level = 4; name = "Dane47"; uuid = "6cfede5c-8117-4673-bd7d-0a17bbab69e2"; } { bypassesPlayerLimit = true; level = 4; name = "ipv6_dotsh"; uuid = "33879815-699c-4a15-b04c-2dce27a570be"; } ]
+    ```
+  */
   toMinecraftOps =
     thots:
     flatten (
@@ -19,4 +50,45 @@ in
         ) thot.minecraftAccounts
       ) thots
     );
+
+  # Unit tests for our lib functions
+  runTests = lib.runTests {
+    test_toMinecraftOps = {
+      expr = toMinecraftOps [
+        {
+          minecraftAccounts = [
+            {
+              name = "Dane47";
+              uuid = "6cfede5c-8117-4673-bd7d-0a17bbab69e2";
+            }
+          ];
+        }
+        {
+          minecraftAccounts = [
+            {
+              name = "ipv6_dotsh";
+              uuid = "33879815-699c-4a15-b04c-2dce27a570be";
+            }
+          ];
+        }
+      ];
+      expected = [
+        {
+          bypassesPlayerLimit = true;
+          level = 4;
+          name = "Dane47";
+          uuid = "6cfede5c-8117-4673-bd7d-0a17bbab69e2";
+        }
+        {
+          bypassesPlayerLimit = true;
+          level = 4;
+          name = "ipv6_dotsh";
+          uuid = "33879815-699c-4a15-b04c-2dce27a570be";
+        }
+      ];
+    };
+  };
+
+  # Can be used to check if tests pass
+  testsPassed = if runTests == [ ] then true else throw "Tests did not pass!";
 }
