98d90f7cfb61fca21d44d993a81f70fdb25c1a1e
Dane Rieber <danerieber@gmail.com>
2025-04-30 17:12:55 -0500

    add wireguardPeers option

--- options.nix
+++ options.nix
@@ -22,23 +22,26 @@ lib: with lib; {
     type = types.listOf (
       types.submodule {
         options = {
+
           name = mkOption {
             description = "User's Minecraft [username](https://minecraft.wiki/w/Player#Username)";
             type = types.strMatching "^[a-zA-Z0-9_]{3,16}";
           };
+
           uuid = mkOption {
             description = "User's Minecraft [UUID](https://minecraft.wiki/w/UUID)";
             type = types.strMatching "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
           };
+
         };
       }
     );
   };
 
   hashedPassword = mkOption {
+    description = "Specifies the hashed password for the user. See {option}`users.users.<name>.hashedPassword`";
     default = null;
     type = with types; nullOr (passwdEntry str);
-    description = "Specifies the hashed password for the user. See {option}`users.users.<name>.hashedPassword`";
   };
 
   shell = mkOption {
@@ -49,6 +52,39 @@ lib: with lib; {
       "fish"
       "zsh"
     ];
+  };
+
+  wireguardPeers = mkOption {
+    description = "Basic information needed to peer with others using WireGuard";
+    default = [ ];
+    type = types.listOf (
+      types.submodule {
+        options = {
+
+          Endpoint = mkOption {
+            description = "Endpoint of WireGuard server";
+            example = "example.com:51820";
+            type = types.str;
+          };
+
+          PublicKey = mkOption {
+            description = "WireGuard public key";
+            example = "AmjREu51OzcQRvwLJ81+Nw0EJr9GmxQlRnUMSZOElBQ=";
+            type = types.str;
+          };
+
+          AllowedIPs = mkOption {
+            description = "List of IPv4/IPv6 CIDRs that other peers are allowed to access through the tunnel";
+            example = [
+              "10.0.0.1/24"
+              "fd00:100::/32"
+            ];
+            type = types.listOf types.str;
+          };
+
+        };
+      }
+    );
   };
 
 }
