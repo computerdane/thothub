4c1efdadc020f3ffe44cf8461ec311ef8ef50e39
John <johnkorhel1@gmail.com>
2025-04-30 20:43:12 -0600

    add config options

---
+++ users/john.nix
@@ -0,0 +1,13 @@
+{
+  githubId = 56358999;
+  sshKeys = {
+    pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC68w7+JGFNPrrB25TiwXGl0bgYBqJJqiLMOqr/bsbp0";
+  };
+  minecraftAccounts = [
+    {
+      name = "Jehova";
+      uuid = "83a425d6-e6bd-44f6-8198-a8fd28ceb060";
+    }
+  ];
+  shell = "fish";
+}
