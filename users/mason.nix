60bcd24598f9c5a0aa167dfd3e40a19a01cbe6c7
mason <58895787+masoniis@users.noreply.github.com>
2025-05-01 07:42:07 -0600

    add users/mason.nix with config

---
+++ users/mason.nix
@@ -0,0 +1,13 @@
+{
+  githubId = 58895787;
+  sshKeys = {
+    mbp14 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZblT7Q/WxYTQnb3WL9lJMclp1DeQeYzdBKOBPAX0bD";
+  };
+  minecraftAccounts = [
+    {
+      name = "shmebop";
+      uuid = "55f8bb1a-f316-4c7e-af74-f8a1366cd57b";
+    }
+  ];
+  shell = "zsh"; # waiting for "sh" to be supported 😔
+}
