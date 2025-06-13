9e6ebffefa5fbb0320b4fd94fafd864226029fcd
Dane Rieber <danerieber@gmail.com>
2025-06-13 09:42:36 -0500

    add user: miles

---
+++ users/miles.nix
@@ -0,0 +1,13 @@
+{
+  githubId = 6487079;
+  sshKeys = {
+    main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEO7krbF2BB9EYFqpVt7DmOdnnDL4g2a0iJWT54C2NMf";
+  };
+  minecraftAccounts = [
+    {
+      name = "Crinklebury";
+      uuid = "10c30d70-21fa-40c1-ace9-3a00623c7dfb";
+    }
+  ];
+  shell = "fish";
+}
